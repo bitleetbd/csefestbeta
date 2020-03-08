1ffb74caaa09f699e301477bd31e8853146f4d7f
bitleetbd <bitleetbd@gmail.com>
2020-03-08 12:40:14 -0700

    Merge branch 'master' of http://github.com/bitleetbd/csefestbeta
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/csemiu/csefestbeta/Venue.java
+++ app/src/main/java/com/csemiu/csefestbeta/Venue.java
@@ -1,14 +1,46 @@
 package com.csemiu.csefestbeta;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import androidx.fragment.app.FragmentActivity;
+
+
+import android.location.Location;
 import android.os.Bundle;
 
-public class Venue extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Venue extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapAPI);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapAPI = googleMap;
+        LatLng ManaratInternationalUniversity = new LatLng(23.8777651,90.3107266);
+        mapAPI.addMarker(new MarkerOptions().position(ManaratInternationalUniversity)
+                .title("Manarat International University"));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLngZoom(ManaratInternationalUniversity, 16f));
+
+
     }
 }
